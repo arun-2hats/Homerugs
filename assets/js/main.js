09c6d162049221aeba1acfb9fbdc96341a8d17a4
Abhinand mc <abhinandmc@2hatslogic.com>
2025-09-23 18:32:11 +0530

    brand slider added

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,32 @@
     },
     effect: 'slide',          // you can also try 'fade'
   });
+
+
+  const brandSwiper = new Swiper('.brand-slider', {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 4,   // default (desktop)
+    spaceBetween: 20,
+    breakpoints: {
+      1024: {
+        slidesPerView: 4, // desktop
+      },
+      768: {
+        slidesPerView: 3, // tablet
+      },
+      480: {
+        slidesPerView: 2, // mobile
+      },
+      0: {
+        slidesPerView: 1, // very small
+      }
+    }
+  });
